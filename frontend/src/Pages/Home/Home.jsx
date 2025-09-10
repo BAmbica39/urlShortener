649f766a979bf8ab8efc8acc976f5acd2780355a
Bejagama Ambica <ambica9415@gmail.com>
2025-09-10 11:54:13 +0530

    second commit

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -53,7 +53,7 @@ const Home = () => {
               marginTop: "1rem",
             }}
           >
-            Your App Name
+            Ambica URL Shortener
           </Title>
 
           <Text
